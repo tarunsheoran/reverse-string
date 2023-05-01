d4c1b8bddb7e449daef7521e201ffa789bfd4ec5
tarunsheoran <118544285+tarunsheoran@users.noreply.github.com>
2023-05-01 11:05:54 +0530

    Add files via upload

---
+++ strrev.c
@@ -0,0 +1,10 @@
+#include <string.h>
+int main() {
+char s1[] ="Tarun";
+char s2[] = "_Sheoram";
+printf("The reversed string s1 is: ");
+puts(strrev(s1));
+printf("The reversed string s2 is: ");
+puts(strrev(s2));
+return 0;
+} 
